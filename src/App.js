c4732736f018f648d9ec855139fc5fe6f4333d52
Hector Matias Reartes <hmatiasreartes5@hotmail.com>
2020-03-10 11:40:45 -0300

    Proyecto Finalizado

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React,{Fragment,useState,useEffect} from 'react';
+import Formulario from './components/Formulario';
+import Cancion from './components/Cancion';
+import Info from './components/Info';
+import axios from 'axios';
+
+function App() {
+
+  //defino el state
+  const [busquedaletra,guardarBusquedaLetra] = useState({});
+  const [letra , guardarLetra]= useState('');
+  const [info, guardarInfo] = useState({});
+
+  useEffect(()=>{
+      if(Object.keys(busquedaletra).length === 0) return;
+      
+      const consultarApiLetra = async () => {
+        const {artista,cancion} = busquedaletra;
+        const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+        const url2 = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artista}`
+
+        //usando promise me permite que ambas consultas inicien al mismo tiempo y de ellas depende cual finaliza primero
+        //esto permite a que una no tenga que estar esperando a que termine la consulta de la otra para recien poder consultar
+        //en pocas palabras ambas se consultan de forma independiente sin estar esperando a que la otra finalice primero para recien ejecutar la consulta
+        const [letra,informacion] = await Promise.all([
+          axios(url),
+          axios(url2)
+        ])
+        guardarLetra(letra.data.lyrics);
+        guardarInfo(informacion.data.artists[0]);
+      }
+      consultarApiLetra();
+  },[busquedaletra,info])
+
+  return (
+    <Fragment>
+      <Formulario 
+          guardarBusquedaLetra={guardarBusquedaLetra}
+      />
+      <div className="container mt-5">
+          <div className="row">
+              <div className="col-md-6">
+                  <Info
+                    info={info}
+                  />
+              </div>
+              <div className="col-md-6">
+                  <Cancion 
+                    letra={letra}
+                  />
+              </div>
+          </div>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
